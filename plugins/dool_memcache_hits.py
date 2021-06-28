e12416c803f0ccc2e44271076be5f8e57656d5c2
Scott Baker <scott.baker@directlink.coop>
2021-06-28 10:26:20 -0700

    Update the memcache plugin

--- plugins/dool_memcache_hits.py
+++ plugins/dool_memcache_hits.py
@@ -1,4 +1,6 @@
 ### Author: Dean Wilson <dean.wilson@gmail.com>
+global socket
+import socket
 
 class dstat_plugin(dstat):
     """
@@ -7,22 +9,66 @@ class dstat_plugin(dstat):
     Displays the number of memcache get_hits and get_misses.
     """
     def __init__(self):
-        self.name = 'Memcache Hits'
+        self.name = 'Memcache'
         self.nick = ('Hit', 'Miss')
         self.vars = ('get_hits', 'get_misses')
         self.type = 'd'
-        self.width = 6
-        self.scale = 50
+        self.width = 4
+        self.scale = 999
 
     def check(self):
-        try:
-            global memcache
-            import memcache
-            self.mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-        except:
-            raise Exception('Plugin needs the memcache module')
+        return 1
 
     def extract(self):
-        stats = self.mc.get_stats()
+        stats = self.get_stats()
+
+        # Store the data we're looking for
         for key in self.vars:
-            self.val[key] = int(stats[0][1][key])
+            self.set2[key] = int(stats.get(key, -1))
+
+        # If we're updating do the math to count how many happened in this interval
+        if update:
+            for key in self.vars:
+                self.val[key] = (self.set2[key] - self.set1[key]) / elapsed
+
+        if step == op.delay:
+            self.set1.update(self.set2)
+
+    def get_stats(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as err:
+            print("socket creation failed with error %s" % (err))
+            sys.exit(2)
+
+        # Check some ENV variables first, then fall back to the defaults
+        port = os.environ.get('DOOL_MEMCACHE_PORT', 11211)
+        host = os.environ.get('DOOL_MEMCACHE_HOST', "127.0.0.1")
+
+        try:
+            s.connect((host, port))
+        except:
+            print("Memcache: Error connecting to %s:%d" % (host, port))
+            sys.exit(1)
+
+        # Send the stats command and then the quit command right after
+        s.sendall(b"stats\n")
+        s.sendall(b"quit\n")
+
+        # Read everything in to a buffer
+        buf = ""
+        while 1:
+            data = s.recv(1024)
+            if not data:
+                break
+            buf += data.decode("utf-8")
+
+        lines = buf.split("\r\n")
+        stats = {}
+
+        for line in lines:
+            parts = line.split(" ")
+            if parts[0] == "STAT":
+                stats[parts[1]] = parts[2]
+
+        return stats
