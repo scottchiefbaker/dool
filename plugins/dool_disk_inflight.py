cea5fec4390f1394899210125e1d1078a40be117
Scott Baker <scott@perturb.org>
2023-10-03 08:49:10 -0700

    Update the inflight plugin per discussion in #53

--- plugins/dool_disk_inflight.py
+++ plugins/dool_disk_inflight.py
@@ -7,13 +7,17 @@ class dool_plugin(dool):
 
     def __init__(self):
         # This is the plugin name that goes on top of the columns
-        self.name  = 'I/O Inflight'
+        #self.name  = 'io/inflight'
+        self.name  = self.get_devs()
+
+        # Title of each column (or group of colums)
+        self.nick = ('ioif',)
         # These are the sub headings under the main column (each disk device)
         self.vars  = self.get_devs()
         # It's a decimal number
         self.type  = 'd'
         # Each column is 7 chars wide
-        self.width = 7
+        self.width = 4
         # Group the colorings to chunks of 20
         self.scale = 20
 
@@ -46,4 +50,4 @@ class dool_plugin(dool):
                 # For testing we can toss random data in the buckets
                 #value = random.randrange(1,100)
 
-                self.val[dev] = value
+                self.val[dev] = (value,)
