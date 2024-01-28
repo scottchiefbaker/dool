92d85277e5a7a85af7e787bbb1217a926d4467b2
Scott Baker <scott@perturb.org>
2024-01-28 13:24:34 -0800

    Better stats per pid

--- plugins/dool__pid_detail.py
+++ plugins/dool__pid_detail.py
@@ -49,7 +49,7 @@ class dool_plugin(dool):
 		cpu_str   = cprint(cpu_usage, 'f', 3, 34)
 		disk_rd   = cprint(disk_rd	, 'd', 5, 1024)
 		disk_wr   = cprint(disk_wr	, 'd', 5, 1024)
-		mem_usage = cprint(mem_usage, 'd', 4, 1024)
+		mem_usage = cprint(mem_usage, 'f', 4, 1024)
 
 		self.output = "%s %s %s %s" % (cpu_str, disk_rd, disk_wr, mem_usage)
 
