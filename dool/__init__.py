29ee5d069f1e9e0ebddfa93782897918eac2021c
Florian Schmaus <flo@geekplace.eu>
2024-09-27 14:36:58 +0200

    Fix and make project structure more Python packaging idiomatic
    The project structure and the pyproject.toml would only cause the
    dool.py script to be installed, not the plugins.
    
    To make it easier for downstream packages, like Gentoo in my case,
    dool should use an idiomatic Python project structure. This change
    moves the files accordingly, adds a __init__.py (as recommended by
    flit), and adjusts pyproject.toml accordingly.

---
+++ dool/__init__.py
@@ -0,0 +1,7 @@
+"""
+Dool is a command line tool to monitor many aspects of your system: CPU,
+Memory, Network, Load Average, etc.  It also includes a robust plug-in
+architecture to allow monitoring other system metrics.
+"""
+
+__version__ = '1.3.3'
--- dool/dool.py
+++ dool/dool.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-Dool is a command line tool to monitor many aspects of your system: CPU,
-Memory, Network, Load Average, etc.  It also includes a robust plug-in
-architecture to allow monitoring other system metrics.
-"""
-
 ### This program is free software; you can redistribute it and/or
 ### modify it under the terms of the GNU General Public License
 ### as published by the Free Software Foundation; either version 2
@@ -41,8 +35,6 @@ import time
 import signal
 
 from collections.abc import Sequence
-
-__version__ = '1.3.3'
 
 theme = { 'default': '' }
 
