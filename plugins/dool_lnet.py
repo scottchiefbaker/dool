98e889e9e380ddf11e484ad12bd62d3ea845ad50
Hans-Nikolai Viessmann <hans-nikolai.viessmann@psi.ch>
2025-02-06 12:11:32 +0100

    add documentation for lnet

--- plugins/dool_lnet.py
+++ plugins/dool_lnet.py
@@ -1,5 +1,14 @@
 # Author: Hans-Nikolai Viessmann <hans-nikolai.viessmann@psi.ch>
 
+# Lustre network utilisation is tricky to capture, as the Lustre
+# driver and the Luster Network driver operate outside of the
+# kernel network stack, i.e. read/write IO on Lustre mount does
+# not report any network activity.
+#
+# Implemnetation is based on stats information collected via
+# LNET and _described_ in the source code, see:
+# https://github.com/lustre/lustre-release/blob/master/lnet/lnet/lnet_debugfs.c
+
 class dool_plugin(dool):
     def __init__(self):
         self.name = 'lnet'
