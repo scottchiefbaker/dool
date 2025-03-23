scottchiefbaker/dool

commit 5068abef0a960c173a91e84a45e7fea1b0a802c0

Clarify what io/bio mean and document where to get more info

plugins/dool_top_io.py
@@ -47,8 +47,11 @@ class dool_plugin(dool):
             else:
                 factor = 1
 
-            read_usage  = (self.pidset2[pid]['read_bytes:']  - self.pidset1[pid]['read_bytes:'])  * factor / elapsed
-            write_usage = (self.pidset2[pid]['write_bytes:'] - self.pidset1[pid]['write_bytes:']) * factor / elapsed
+            # INFO: https://www.kernel.org/doc/html/latest/filesystems/proc.html#proc-pid-io-display-the-io-accounting-fields
+            #
+            ### 'rchar' counts bytes read from the task POV, e.g. open files which may be read from page cache, reading from a socket or pipe
+            read_usage  = (self.pidset2[pid]['rchar:'] - self.pidset1[pid]['rchar:']) * factor / elapsed
+            write_usage = (self.pidset2[pid]['wchar:'] - self.pidset1[pid]['wchar:']) * factor / elapsed
             usage       = read_usage + write_usage
 
             ### Get the process that spends the most jiffies

plugins/dool_top_bio.py
@@ -42,9 +42,12 @@ class dool_plugin(dool):
             except IndexError:
                 continue
 
-            read_usage = (self.pidset2[pid]['read_bytes:'] - self.pidset1[pid]['read_bytes:']) * 1.0 / elapsed
+            # INFO: https://www.kernel.org/doc/html/latest/filesystems/proc.html#proc-pid-io-display-the-io-accounting-fields
+            #
+            ### 'read_bytes' counts bytes read from the storage layer, i.e. when the block device driver is used
+            read_usage  = (self.pidset2[pid]['read_bytes:']  - self.pidset1[pid]['read_bytes:'])  * 1.0 / elapsed
             write_usage = (self.pidset2[pid]['write_bytes:'] - self.pidset1[pid]['write_bytes:']) * 1.0 / elapsed
-            usage = read_usage + write_usage
+            usage       = read_usage + write_usage
 
             ### Get the process that spends the most jiffies
             if usage > self.val['usage']:

commit 0482fc138093a8d844db047bd096b0ff05606f38

Fix crash when using --top-io plugin
Use the same field names during reset, otherwise dool crashes with a
KeyError due to a missing `rchar:` dict key.

Fixes: 5068abef0a96 ("Clarify what io/bio mean and document where to get more info")

plugins/dool_top_io.py
@@ -26,9 +26,9 @@ class dool_plugin(dool):
             try:
                 ### Reset values
                 if pid not in self.pidset2:
-                    self.pidset2[pid] = {'read_bytes:': 0, 'write_bytes:': 0}
+                    self.pidset2[pid] = {'rchar:': 0, 'wchar:': 0}
                 if pid not in self.pidset1:
-                    self.pidset1[pid] = {'read_bytes:': 0, 'write_bytes:': 0}
+                    self.pidset1[pid] = {'rchar:': 0, 'wchar:': 0}
 
                 ### Extract name
                 name = proc_splitline('/proc/%s/stat' % pid)[1][1:-1]