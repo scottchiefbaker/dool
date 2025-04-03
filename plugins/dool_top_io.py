scottchiefbaker/dool

commit db330372733b9f79a67acdcea5219576356281dc

Skip files we cannot get /proc/$PID/io data from (Fixes #94?)
Usually this is low numbered pids owned by root (and we're a normal
user)

plugins/dool_top_io.py
@@ -23,6 +23,8 @@ class dool_plugin(dool):
         self.pidset2      = {}
         self.val['usage'] = 0.0
         for pid in proc_pidlist():
+            proc_file = ('/proc/%s/io' % (pid))
+
             try:
                 ### Reset values
                 if pid not in self.pidset2:
@@ -31,10 +33,10 @@ class dool_plugin(dool):
                     self.pidset1[pid] = {'read_bytes:': 0, 'write_bytes:': 0}
 
                 ### Extract name
-                name = proc_splitline('/proc/%s/stat' % pid)[1][1:-1]
+                name = get_name_by_pid(pid)
 
                 ### Extract counters
-                for l in proc_splitlines('/proc/%s/io' % pid):
+                for l in proc_splitlines(proc_file):
                     if len(l) != 2: continue
                     self.pidset2[pid][l[0]] = int(l[1])
             except IOError:
@@ -46,6 +48,11 @@ class dool_plugin(dool):
                 factor = 8
             else:
                 factor = 1
+
+            has_rchar = self.pidset1[pid].get("rchar:", False)
+
+            if not has_rchar:
+                continue
 
             # INFO: https://www.kernel.org/doc/html/latest/filesystems/proc.html#proc-pid-io-display-the-io-accounting-fields
             #

commit 2bd423b7ac29cef998f230aa3fd38a06fb7e9a8e

Fix empty row for the first iteration of --top-io
Stats from non-root processes cannot be read and should be skipped. On
the first iteration, we historically reported the processes with the
most I/O, so let's restore that functionality.

Reduce memory consumption by not storing unused fields, and skip reading
process names when the counters cannot be read anyway. Refactor the code
to make it easier to review, some exceptions could never occur.

Fixes: db330372733b ("Skip files we cannot get /proc/$PID/io data from (Fixes #94?)")

plugins/dool_top_io.py
@@ -22,37 +22,32 @@ class dool_plugin(dool):
         self.output       = ''
         self.pidset2      = {}
         self.val['usage'] = 0.0
+        empty_counters = {'rchar:': 0, 'wchar:': 0}
+        mandatory_fields = empty_counters.keys()
         for pid in proc_pidlist():
-            proc_file = ('/proc/%s/io' % (pid))
-
-            try:
-                ### Reset values
-                if pid not in self.pidset2:
-                    self.pidset2[pid] = {'read_bytes:': 0, 'write_bytes:': 0}
-                if pid not in self.pidset1:
-                    self.pidset1[pid] = {'read_bytes:': 0, 'write_bytes:': 0}
-
-                ### Extract name
-                name = get_name_by_pid(pid)
-
-                ### Extract counters
-                for l in proc_splitlines(proc_file):
-                    if len(l) != 2: continue
-                    self.pidset2[pid][l[0]] = int(l[1])
-            except IOError:
+            ### Extract counters
+            newdata = {}
+            proc_file = f'/proc/{pid}/io'
+            for l in proc_splitlines(proc_file):
+                if len(l) != 2: continue
+                if l[0] not in mandatory_fields: continue
+                newdata[l[0]] = int(l[1])
+            # Output can be missing when reading process info for other users.
+            if len(newdata) != len(mandatory_fields):
                 continue
-            except IndexError:
-                continue
+
+            ### Extract name
+            name = get_name_by_pid(pid)
+
+            # New process? Pretend the counters started at zero.
+            if pid not in self.pidset1:
+                self.pidset1[pid] = empty_counters
+            self.pidset2[pid] = newdata
 
             if (op.bits):
                 factor = 8
             else:
                 factor = 1
-
-            has_rchar = self.pidset1[pid].get("rchar:", False)
-
-            if not has_rchar:
-                continue
 
             # INFO: https://www.kernel.org/doc/html/latest/filesystems/proc.html#proc-pid-io-display-the-io-accounting-fields
             #