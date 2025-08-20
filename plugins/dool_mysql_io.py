63ac435a04fe6fdff475ea352e1565a403d0a6b5
Bastian Triller <bastian.triller@gmail.com>
2025-08-20 15:41:39 +0200

    plugins/mysql5-innodb: Replace dstat leftovers

--- plugins/dool_mysql_io.py
+++ plugins/dool_mysql_io.py
@@ -37,7 +37,7 @@ class dool_plugin(dool):
             for name in self.vars: self.val[name] = -1
 
         except Exception as e:
-            if op.debug > 1: print('dstat_innodb_buffer: exception', e)
+            if op.debug > 1: print('dool_mysql_io: exception', e)
             for name in self.vars: self.val[name] = -1
 
 # vim:ts=4:sw=4:et
