88acfa009f01e8e7d2f1f7958f39837637eb4b3e
Scott Baker <scott@perturb.org>
2025-08-20 16:27:58 -0700

    Clean up the test plugin so it's simpler

--- plugins/dool_test.py
+++ plugins/dool_test.py
@@ -4,19 +4,15 @@ class dool_plugin(dool):
     '''
     Provides a test playground to test syntax and structure.
     '''
+
     def __init__(self):
         self.name = 'test'
-        self.nick = ( 'f1', 'f2' )
         self.vars = ( 'f1', 'f2' )
-#        self.type = 'd'
-#        self.width = 4
-#        self.scale = 20
         self.type = 's'
         self.width = 4
         self.scale = 0
 
     def extract(self):
-#        Self.val = { 'f1': -1, 'f2': -1 }
         self.val = { 'f1': 'test', 'f2': 'test' }
 
 # vim:ts=4:sw=4:et
