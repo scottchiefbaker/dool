01bba9452c912ec719ed04a9281540fb54a35839
Scott Baker <scott@perturb.org>
2025-09-04 14:12:30 -0700

    Make sure **ALL** building is done in `/var/tmp/`

--- packaging/rpm/build.sh
+++ packaging/rpm/build.sh
@@ -29,9 +29,9 @@ sleep 1
 
 # Build a tar archive of the current HEAD with the correct version number
 cd ../../
-git archive --format=tar --prefix=dool-$VERSION/ HEAD | gzip > /tmp/dool-$VERSION.tar.gz
+git archive --format=tar --prefix=dool-$VERSION/ HEAD | gzip > /var/tmp/dool-$VERSION.tar.gz
 mkdir -p ~/rpmbuild/SOURCES/
-cp -a /tmp/dool-$VERSION.tar.gz ~/rpmbuild/SOURCES/
+cp -a /var/tmp/dool-$VERSION.tar.gz ~/rpmbuild/SOURCES/
 cd -
 
 # Build the actual RPM
