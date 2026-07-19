ddbd3671e3a76901640451dc477c508161218c7f
fazalpsinfo-cmyk <fazalpsinfo@gmail.com>
2026-07-19 17:11:56 +0530

    Fix bare except in dool_vmk_int.py

--- plugins/dool_vmk_int.py
+++ plugins/dool_vmk_int.py
@@ -74,7 +74,7 @@ class dool_plugin(dool):
     def check(self): 
         try:
             os.listdir('/proc/vmware')
-        except:
+        except Exception:
             raise Exception('Needs VMware ESX')
         info(1, 'The vmkint module is an EXPERIMENTAL module.')
 
